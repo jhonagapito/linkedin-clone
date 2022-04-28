554c5c7fcb909fd253b3cf71deb6db2b1e1ce6ce
Jhon-Agapito <jhonea@bss-ph.com>
2022-04-28 22:26:15 +0800

    wrap .map in jsx fragments

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,57 +1,53 @@
 import React from 'react'
-import InfoRoundedIcon from "@mui/icons-material/InfoRounded";
-import FiberManualRecordRoundedIcon from "@mui/icons-material/FiberManualRecordRounded";
-import TimeAgo from "timeago-react";
-import Image from 'next/image';
+import InfoRoundedIcon from '@mui/icons-material/InfoRounded'
+import FiberManualRecordRoundedIcon from '@mui/icons-material/FiberManualRecordRounded'
+import TimeAgo from 'timeago-react'
+import Image from 'next/image'
 import widgetAd from '../public/assets/widgetAd.png'
 interface Props {
   articles: Array<any>
 }
 
-const Widgets = ({ articles } : Props) => {
+const Widgets = ({ articles }: Props) => {
   console.log(articles)
   const openArticle = (link: string) => {
-    window.open(link,'_blank');
+    window.open(link, '_blank')
   }
   return (
-    <div className="hidden xl:inline space-y-2">
-      <div className="bg-white dark:bg-[#1D2226] py-2.5 rounded-lg space-y-2 w-11/12 overflow-hidden border border-gray-300 dark:border-none">
-        <div className="flex items-center justify-between font-bold px-2.5">
-            <h4>LinkedIn News</h4>
-            <InfoRoundedIcon className='h-5 w-5' />
+    <div className="hidden space-y-2 xl:inline">
+      <div className="w-11/12 space-y-2 overflow-hidden rounded-lg border border-gray-300 bg-white py-2.5 dark:border-none dark:bg-[#1D2226]">
+        <div className="flex items-center justify-between px-2.5 font-bold">
+          <h4>LinkedIn News</h4>
+          <InfoRoundedIcon className="h-5 w-5" />
         </div>
 
         <div className="space-y-1">
-        {
-          articles.slice(0, 5).map((article) => (
-            <div 
-            onClick={() => openArticle(article.url)}
-            key={article.url}
-            className="flex space-x-2 items-center cursor-pointer hover:bg-black/10 dark:hover:bg-black/20 px-2.5 py-1">
-                <FiberManualRecordRoundedIcon className='!h-2 !w-2' />
+          <>
+            {articles.slice(0, 5).map((article) => (
+              <div
+                onClick={() => openArticle(article.url)}
+                key={article.url}
+                className="flex cursor-pointer items-center space-x-2 px-2.5 py-1 hover:bg-black/10 dark:hover:bg-black/20"
+              >
+                <FiberManualRecordRoundedIcon className="!h-2 !w-2" />
                 <div>
-                  <h5 className='max-w-xs font-medium text-sm truncate pr-10'>
-                    { article.title }
+                  <h5 className="max-w-xs truncate pr-10 text-sm font-medium">
+                    {article.title}
                   </h5>
-                  <TimeAgo 
+                  <TimeAgo
                     datetime={article.publishedAt}
-                    className='text-xs mt-0.5 dark:text-white/75 opacity-80'
+                    className="mt-0.5 text-xs opacity-80 dark:text-white/75"
                   />
                 </div>
-            </div>
-          ))
-        }
+              </div>
+            ))}
+          </>
         </div>
       </div>
-      <div className="bg-white dark:bg-[#1D2226] w-11/12 h-64 px-2.5 rounded-lg sticky top-20 border border-gray-300 dark:border-none">
-          <div className="relative w-full h-full">
-            <Image 
-              src={widgetAd}
-              layout='fill'
-              objectFit='contain'
-              priority
-            />
-          </div>
+      <div className="sticky top-20 h-64 w-11/12 rounded-lg border border-gray-300 bg-white px-2.5 dark:border-none dark:bg-[#1D2226]">
+        <div className="relative h-full w-full">
+          <Image src={widgetAd} layout="fill" objectFit="contain" priority />
+        </div>
       </div>
     </div>
   )
